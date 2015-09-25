df33cdb336cc9f3013fee787a8cdb115f94f64ff
Edualuc <edualuc@gmail.com>
2015-09-24 22:47:32 -0300

    Edu ok

--- app/src/androidTest/java/com/ufrgs/ufrgs/ApplicationTest.java
+++ app/src/androidTest/java/com/ufrgs/ufrgs/ApplicationTest.java
@@ -3,7 +3,7 @@ package com.ufrgs.ufrgs;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
+/** Eduardo ok
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
